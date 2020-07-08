80953565c0cceeb51197654f66a58b0cce20bf18
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-08 14:54:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab_2._3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
