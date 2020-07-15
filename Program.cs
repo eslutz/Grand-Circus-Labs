fc46b6856e1fd138a30b8f9d4d62af9b2bf89188
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-15 14:33:18 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab_3._3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter a word you would like to reverse. => ");
+			string word = Console.ReadLine();
+		}
+	}
+}
