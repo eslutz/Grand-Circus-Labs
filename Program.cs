b2a0ee3fedb34c8c86a57ceca2051420ed4d1cd8
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-22 16:21:28 -0400

    Completed the project except for extended challenge.  Working on looping until the user quits and sorting the list.

--- Program.cs
+++ Program.cs
@@ -13,13 +13,30 @@ namespace Lab_4._3
 			List<string> categories = GetCategories(movieList);
 
 			Console.WriteLine("Welcome to the Movie List Application!\n");
-			Console.WriteLine($"There are {movieList.Count} movies in the list of the following categories:");
-			for(int index = 0; index < categories.Count; index++)
+
+			do
 			{
-				Console.WriteLine($"{index + 1}. {categories[index]}");
-			}
-			Console.Write("What category are you interested int? => ");
-			string input = Console.ReadLine();
+				//Shows how many movies and a list of the available categories.
+				Console.WriteLine($"There are {movieList.Count} movies in the list of the following categories:");
+				for (int index = 0; index < categories.Count; index++)
+				{
+					Console.WriteLine($"{index + 1}. {categories[index]}");
+				}
+
+				//Gets input from the user and validates it for their category choice
+				Console.Write($"What category are you interested in? Please select a category 1 - {categories.Count} => ");
+				bool isValid = int.TryParse(Console.ReadLine(), out int input);
+				while (!isValid || !(input > 0 && input <= categories.Count))
+				{
+					Console.WriteLine("That is not a valid category.");
+					Console.Write($"Please select a category 1 - {categories.Count} => ");
+					isValid = int.TryParse(Console.ReadLine(), out input);
+				}
+
+				//Calls method to display the movies, passing the list of movies and the chosen category.
+				DisplayMovies(movieList, categories[input - 1]);
+
+			} while (true);
 		}
 
 		//Generates a list of movies and returns the list.
@@ -45,7 +62,7 @@ namespace Lab_4._3
 			movies.Add(new Movies("Gladiator", "Drama"));
 			movies.Add(new Movies("Hidden Figures", "Drama"));
 			movies.Add(new Movies("Idiocracy", "Documentary"));
-			movies.Add(new Movies("Indiana Jones and the Last Crusade", "Action"));
+			movies.Add(new Movies("Indiana Jones", "Action"));
 			movies.Add(new Movies("John Wick", "Action"));
 			movies.Add(new Movies("Joker", "Drama"));
 			movies.Add(new Movies("Jurassic Park", "Action"));
@@ -77,7 +94,40 @@ namespace Lab_4._3
 				}
 			}
 			//Returns the list of unique categories.
+			categories.Add("All");
 			return categories;
+		}
+
+		//Displays the movies in the chosen category.
+		public static void DisplayMovies(List<Movies> movies, string category)
+		{
+			//Puts each movies of the chosen category into a list.
+			List<Movies> sortedMovies = new List<Movies>();
+			if (category == "All")
+			{
+				sortedMovies = movies;
+			}
+			else
+			{
+				foreach (Movies movie in movies)
+				{
+					if (movie.Category == category)
+					{
+						sortedMovies.Add(movie);
+					}
+				}				
+			}
+
+			//Sorted movies by title using LINQ.
+			sortedMovies.OrderBy(x => x.Title);
+
+			//Displays list of movies of the chosen category.
+			Console.WriteLine($"\n{"Title",-25}{"Category",-25}");
+			Console.WriteLine(new String('*', 50));
+			foreach(Movies movie in sortedMovies)
+			{
+				Console.WriteLine($"{movie.Title,-25}{movie.Category,-25}");
+			}
 		}
 	}
 }
