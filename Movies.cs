75a827d477cf5ac2455033158e3f43f226bdc36b
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-22 14:57:38 -0400

    Add project files.

---
+++ Movies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_4._3
+{
+	class Movies
+	{
+		//Fields
+		private string _title;
+		private string _category;
+
+		//
+		public string Title
+		{
+			get { return _title; }
+		}
+
+		public string Category
+		{
+			get { return _category; }
+		}
+
+		//Constructor
+		public Movies(string title, string category)
+		{
+			_title = title;
+			_category = category;
+		}
+	}
+}
