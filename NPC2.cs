5b681103634d91c2ee114223b15ce876d4c8e442
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-29 14:52:59 -0400

    Created empty classes and setup inheritance.

---
+++ NPC2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_5._2
+{
+	class NPC2 : Player
+	{
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_5._2
+{
+	abstract class Player
+	{
+		
+	}
+}
