86f6904d852f7394b3e40115ab31225659a80f97
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-29 14:47:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_5._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the Grand Circus Rock Paper Scissors World Chamionship!");
+		}
+	}
+}
