c87dbf8cda6f4d8b8025011df34f3d858aa1c084
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-30 14:01:56 -0400

    Added comments to NPC1 class.

--- NPC1.cs
+++ NPC1.cs
@@ -6,11 +6,13 @@ namespace Lab_5._2
 {
 	class NPC1 : Player
 	{
+		//Constructor updates the name for the rock only player.
 		public NPC1() : base()
 		{
 			base.Name = "DumbDumb";
 		}
 
+		//Updates players Roshambo value and always returns rock.
 		public override RPS GenerateRoshambo()
 		{
 			base.Roshambo = RPS.rock;
