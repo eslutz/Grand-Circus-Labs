1e4f1775a130f67bb40f644f6c557183fc03d226
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-08-07 14:25:18 -0400

    Updated tests for Validator class.

--- Lab 6.2.Test/ValidatorTest.cs
+++ Lab 6.2.Test/ValidatorTest.cs
@@ -7,64 +7,23 @@ namespace Lab_6._2.Test
 {
 	public class ValidatorTest
 	{
-		[Fact]
-		public void YesNoTest1()
+		[Theory]
+		[InlineData("Yes", YesNo.Yes)]
+		[InlineData("Y", YesNo.Yes)]
+		[InlineData("yes", YesNo.Yes)]
+		[InlineData("y", YesNo.Yes)]
+		[InlineData("No", YesNo.No)]
+		[InlineData("N", YesNo.No)]
+		[InlineData("no", YesNo.No)]
+		[InlineData("n", YesNo.No)]
+		public void YesNoTest1(string input, YesNo expected)
 		{
-			Assert.True(Validator.YesOrNo("Yes", out YesNo test));
-			Assert.Equal(YesNo.Yes, test);
+			Assert.True(Validator.YesOrNo(input, out YesNo test));
+			Assert.Equal(expected, test);
 		}
 
 		[Fact]
 		public void YesNoTest2()
-		{
-			Assert.True(Validator.YesOrNo("yes", out YesNo test));
-			Assert.Equal(YesNo.Yes, test);
-		}
-
-		[Fact]
-		public void YesNoTest3()
-		{
-			Assert.True(Validator.YesOrNo("Y", out YesNo test));
-			Assert.Equal(YesNo.Yes, test);
-		}
-
-		[Fact]
-		public void YesNoTest4()
-		{
-			Assert.True(Validator.YesOrNo("y", out YesNo test));
-			Assert.Equal(YesNo.Yes, test);
-		}
-
-		[Fact]
-		public void YesNoTest5()
-		{
-			Assert.True(Validator.YesOrNo("No", out YesNo test));
-			Assert.Equal(YesNo.No, test);
-		}
-
-		[Fact]
-		public void YesNoTest6()
-		{
-			Assert.True(Validator.YesOrNo("no", out YesNo test));
-			Assert.Equal(YesNo.No, test);
-		}
-
-		[Fact]
-		public void YesNoTest7()
-		{
-			Assert.True(Validator.YesOrNo("N", out YesNo test));
-			Assert.Equal(YesNo.No, test);
-		}
-
-		[Fact]
-		public void YesNoTest8()
-		{
-			Assert.True(Validator.YesOrNo("n", out YesNo test));
-			Assert.Equal(YesNo.No, test);
-		}
-
-		[Fact]
-		public void YesNoTest9()
 		{
 			Assert.False(Validator.YesOrNo("asdfjhjk73&", out YesNo test));
 			Assert.Equal(YesNo.NotValid, test);
