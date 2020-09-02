24dda76550830d22d50947758843d113fc182dcf
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-01 21:57:23 -0400

    Added validation of input.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
@@ -23,7 +24,11 @@ namespace Lab_11._2.Controllers
 
 		public ActionResult Registered(string firstname, string lastname, DateTime birthday, string email, string phonenumber, string state, string password)
 		{
-			if (true)
+			Regex validName = new Regex(@"^[A-Z][A-Za-z]{0,29}$");
+			Regex validEmail = new Regex(@"^.{5,30}@\w{5,10}\.\w{2,3}$");
+			Regex validPhonenumber = new Regex(@"^\({0,1}\d{3}\){0,1}[-.]\d{3}[-.]\d{4}$");
+
+			if (validName.IsMatch(firstname) && validName.IsMatch(lastname) && validEmail.IsMatch(email) && validPhonenumber.IsMatch(phonenumber))
 			{
 				ViewBag.ValidRegister = "You have <b>sucessfully</b> registered!";
 				ViewBag.Message = $"Welcome <b>{firstname} {lastname}</b> with a b-day on <b>{birthday.ToShortDateString()}</b>!<br />We'll spam you constantly at <b>{email}</b> and text you at <b>{phonenumber}</b>.<br />We're also going to show up at your home in <b>{state}</b>.<br />On a final note, I know your password is <b>{password}</b> so ha!";
