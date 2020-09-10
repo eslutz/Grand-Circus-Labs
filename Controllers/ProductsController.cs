e75a4b1fe35d5e9aa1656255a3b30f902781d54d
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-10 17:02:43 -0400

    Added database connection and installed dapper and sqlclient packages.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Lab_13._2.Controllers
 {
@@ -10,6 +13,11 @@ namespace Lab_13._2.Controllers
 	{
 		public IActionResult Index()
 		{
+			IDbConnection database = new SqlConnection("Server=BCKW433\\SQLEXPRESS;Database=CoffeeShop;user id=CoffeeShopUser;password=password");
+			database.Open();
+			
+			database.Close();
+
 			return View();
 		}
 	}
