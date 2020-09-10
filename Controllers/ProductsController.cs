20d4b7639d115078426bca5118e9a297f58fd5a9
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-10 17:19:06 -0400

    Read in info from database and got it listed on product page.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Dapper;
 using System.Data;
 using System.Data.SqlClient;
+using Lab_13._2.Models;
 
 namespace Lab_13._2.Controllers
 {
@@ -15,10 +16,10 @@ namespace Lab_13._2.Controllers
 		{
 			IDbConnection database = new SqlConnection("Server=BCKW433\\SQLEXPRESS;Database=CoffeeShop;user id=CoffeeShopUser;password=password");
 			database.Open();
-			
+			List<Product> products = database.Query<Product>("select * from Product").AsList<Product>();
 			database.Close();
 
-			return View();
+			return View(products);
 		}
 	}
 }
