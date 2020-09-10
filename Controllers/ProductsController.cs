7864981cd51dcb284a279de409a05e42cf45d12c
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-10 16:27:03 -0400

    Converted previous registration Coffee Shop to Core MVC.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_13._2.Controllers
+{
+	public class ProductsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Models/TheUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_13._2.Models
+{
+	public class TheUser
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Gender { get; set; }
+		public DateTime Birthday { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+		public string State { get; set; }
+		public string Password { get; set; }
+	}
+}
