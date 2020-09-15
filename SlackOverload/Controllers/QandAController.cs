32f22d1199eaf907f58584e5eea92a477c064f27
Manny <mannysalazar@quickenloans.com>
2020-09-15 15:36:50 -0400

    Add project files.

---
+++ SlackOverload/Controllers/QandAController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dapper.Contrib.Extensions;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace SlackOverload.Controllers
+{
+    public class QandAController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
