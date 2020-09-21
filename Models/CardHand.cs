eea94686848a1fb94200583e7073a63b95ed9262
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-21 15:14:21 -0400

    Added models for the deck, hand, and card.

---
+++ Models/CardHand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_15._1.Models
+{
+	public class CardHand
+	{
+		public bool GreatSuccess { get; set; }
+		public CardDetail[] Cards { get; set; }
+		public string Deck_ID { get; set; }
+		public int Remaining { get; set; }
+	}
+}
