0d03cec9a001ea03f2f858be892c63a2af98cee9
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-22 15:29:16 -0400

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_15._2.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class MoviesController : ControllerBase
+	{
+	}
+}
