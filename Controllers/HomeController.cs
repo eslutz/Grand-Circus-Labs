1e75eb6165084e3f657f2d54011aff4cc21cf76e
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-21 20:26:33 -0400

    Attempted to add keep card feature, but couldn't get it working.  The rest of the program is still good to go.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,8 +24,17 @@ namespace Lab_15._1.Controllers
 			return View();
 		}
 
-		public async Task<IActionResult> DeckOfCards(string drawAgain, string deck_id)
+		public async Task<IActionResult> DeckOfCards(string drawAgain, string deck_id/*, string card0, string card1, string card2, string card3, string card4*/)
 		{
+			//List<string> keep = new List<string>() { card0, card1, card2, card3, card4};
+			//bool keepCard = false;
+			//foreach(string card in keep)
+			//{
+			//	if (!string.IsNullOrEmpty(card))
+			//	{
+			//		keepCard = true;
+			//	}
+			//}
 			HttpClient client = new HttpClient();
 			client.BaseAddress = new Uri("https://deckofcardsapi.com");
 			var response = await client.GetAsync("/api/deck/new/shuffle");
@@ -36,6 +45,10 @@ namespace Lab_15._1.Controllers
 				response = await client.GetAsync($"/api/deck/{deck.Deck_ID}/draw/?count=5");
 				hand = await response.Content.ReadAsAsync<CardHand>();
 			}
+			//else if(keepCard)
+			//{
+			//	response = await client.GetAsync($"/api/deck/{deck_id}/pile/keepCards/add/?cards=AS,2S");
+			//}
 			else
 			{
 				response = await client.GetAsync($"/api/deck/{deck_id}/draw/?count=5");
