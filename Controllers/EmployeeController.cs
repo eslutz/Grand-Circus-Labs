a0d5c65c80e854c8e78f0c76905fc7f406dbb73b
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-23 16:51:12 -0400

    Added an add employee function.

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -18,6 +18,37 @@ namespace Lab_15._3.Controllers
 			_database = database;
 		}
 
+		//Default route returns list of all employees by default
+		[HttpGet]
+		public List<Employee> Get()
+		{
+			List<Employee> employees = Employee.Read(_database);
+			return employees;
+		}
 
+		//Employee/Country returns list of employees ordered by country
+		//Employee/Country?country=<<user input>> returns list of employees in that country
+		[HttpGet("Country")]
+		public List<Employee> EmployeeCountry(string country)
+		{
+			List<Employee> employees;
+			if (country == null)
+			{
+				employees = Employee.Read(_database).OrderBy(x => x.Country).ToList();
+
+			}
+			else
+			{
+				employees = Employee.Read(_database).Where(x => x.Country == country).ToList();
+			}
+			return employees;
+		}
+
+		[HttpPost("AddEmployee")]
+		public long AddEmployee(string lastname, string firstname)
+		{
+			long employeeID = Employee.Create(_database, lastname, firstname);
+			return employeeID;
+		}
 	}
 }
--- Employee.cs
+++ Employee.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +9,12 @@ namespace Lab_15._3
 {
 	public class Employee
 	{
+		[Key]
 		public long EmployeeID { get; set; }
 		public string LastName { get; set; }
 		public string FirstName { get; set; }
 		public string Title { get; set; }
-		public string TitleOfCourtesey { get; set; }
+		public string TitleOfCourtesy { get; set; }
 		public DateTime BirthDate { get; set; }
 		public DateTime HireDate { get; set; }
 		public string Address { get; set; }
@@ -20,8 +23,36 @@ namespace Lab_15._3
 		public string Country { get; set; }
 		public string HomePhone { get; set; }
 		public string Extension { get; set; }
-		public string Photo { get; set; }
-		public long ReportsTo { get; set; }
+		public string ReportsTo { get; set; }
 		public string PhotoPath { get; set; }
+
+		public static List<Employee> Read(IDbConnection database)
+		{
+			List<Employee> employees = database.GetAll<Employee>().ToList();
+			return employees;
+		}
+
+		public static long Create(IDbConnection database, string lastname, string firstname)
+		{
+			Employee employee = new Employee()
+			{
+				FirstName = "test",
+				LastName = lastname,
+				Title = null,
+				TitleOfCourtesy = null,
+				BirthDate = DateTime.Now,
+				HireDate = DateTime.Now,
+				Address = null,
+				City = null,
+				Region = null,
+				Country = null,
+				HomePhone = null,
+				Extension = null,
+				ReportsTo = null,
+				PhotoPath = null
+			};
+			long id = database.Insert<Employee>(employee);
+			return id;
+		}
 	}
 }
