28f7c9013650a3e5999f0160d792efbf40fc142d
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-23 15:39:14 -0400

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_15._3.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class EmployeeController : ControllerBase
+	{
+		private IDbConnection _database;
+		public EmployeeController(IDbConnection database)
+		{
+			_database = database;
+		}
+
+
+	}
+}
---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_15._3
+{
+	public class Employee
+	{
+		public long EmployeeID { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public string Title { get; set; }
+		public string TitleOfCourtesey { get; set; }
+		public DateTime BirthDate { get; set; }
+		public DateTime HireDate { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string Region { get; set; }
+		public string Country { get; set; }
+		public string HomePhone { get; set; }
+		public string Extension { get; set; }
+		public string Photo { get; set; }
+		public long ReportsTo { get; set; }
+		public string PhotoPath { get; set; }
+	}
+}
