875775859b8c4cd245df229de15eafee4e180729
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-10-14 15:36:39 -0400

    Built out services.

--- src/app/services/dev.service.ts
+++ src/app/services/dev.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Devs } from '../interfaces/devs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DevService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDevs() {
+    return this.http.get<Devs>("https://grandcircusco.github.io/demo-apis/computer-science-hall-of-fame.json");
+  }
 }
--- src/app/services/donut.service.ts
+++ src/app/services/donut.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DonutDetails } from '../interfaces/donut-details';
+import { Donuts } from '../interfaces/donuts';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonutService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDonuts() {
+    return this.http.get<Donuts>("https://grandcircusco.github.io/demo-apis/donuts.json");
+  }
+
+  getDonutDetails(url: string) {
+    return this.http.get<DonutDetails>(url);
+  }
 }
