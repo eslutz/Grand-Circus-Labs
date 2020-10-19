54e681f06f32c4cdc8449ca4fd2b9a83c1fa29e6
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-10-18 20:38:42 -0400

    Added a home page to start navigation from.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,11 @@ import { DonutDetailsComponent } from './donut-details/donut-details.component';
 import { DonutsComponent } from './donuts/donuts.component';
 import { FamousPeopleComponent } from './famous-people/famous-people.component';
 import { FamousPersonDetailsComponent } from './famous-person-details/famous-person-details.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: "", redirectTo: "Home", pathMatch: "full" },
+  { path: "Home", component: HomeComponent },
   { path: "Donuts", component: DonutsComponent },
   { path: "DonutDetails", component: DonutDetailsComponent },
   { path: "Devs", component: FamousPeopleComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DonutsComponent } from './donuts/donuts.component';
 import { DonutDetailsComponent } from './donut-details/donut-details.component';
 import { FamousPersonDetailsComponent } from './famous-person-details/famous-person-details.component';
 import { FamousPeopleComponent } from './famous-people/famous-people.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FamousPeopleComponent } from './famous-people/famous-people.component';
     DonutsComponent,
     DonutDetailsComponent,
     FamousPersonDetailsComponent,
-    FamousPeopleComponent
+    FamousPeopleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
